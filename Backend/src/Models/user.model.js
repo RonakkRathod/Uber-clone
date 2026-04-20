a7134ff50fd7cc2505989a828fe5c637c4d2c361
Ronak <ronakrathod0022@gmail.com>
2026-04-20 23:24:22 +0530

    add user model with authentication methods and password hashing using bcrypt

---
+++ Backend/src/Models/user.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        firstName :{
+            type: String,
+            required: true,
+            minlength: [3,"First name should be at least 3 characters long"]
+        },
+        lastName :{
+            type: String,
+            minlength: [3,"Last name should be at least 3 characters long"]
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password :{
+        type: String,
+        required: true,
+        minlength: [6,"Password should be at least 6 characters long"],
+        select: false
+    },
+    socketId: {
+        type: String
+    }
+})
+
+userSchema.pre("save", async function () {
+    if(!this.isModified("password")) return;
+
+    this.password = await bcrypt.hash(this.password, 10)
+    return;
+}) 
+
+userSchema.methods.generateAuthToken = function () {
+     return jwt.sign(
+        {
+            _id : this._id,
+        },
+     process.env.AUTH_TOKEN_SECRET,
+     {
+        expiresIn : process.env.AUTH_TOKEN_EXPIRY
+     }
+    )
+}
+
+userSchema.methods.comparePassword = async function (password){
+    return await bcrypt.compare(password, this.password);
+}
+
+
+export const User = mongoose.model("User",userSchema);
