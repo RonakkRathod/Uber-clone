d1bda777912c0fa7bff80c03db4aba130966d93b
Ronak <ronakrathod0022@gmail.com>
2026-04-20 23:23:54 +0530

    integrate user routes and enhance express middleware setup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,10 +3,13 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors"
-
+import UserRouter from "./routes/user.routes.js";
 
 const app = express();
+
+app.use(express.json({ limit: "16kb" }));
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 7000;
 
@@ -14,5 +17,6 @@ app.get("/", (req, res) => {
   res.send("Hello Ronak!");
 });
 
+app.use("/api/v1/users", UserRouter);
 
 export default app;
