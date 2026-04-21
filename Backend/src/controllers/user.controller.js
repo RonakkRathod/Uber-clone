1f83e89014c28e9654f3511eaef1c524f7df5fe7
Ronak <ronakrathod0022@gmail.com>
2026-04-21 14:59:02 +0530

    add generateAuthToken function and enhance loginUser with cookie handling and error responses

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import { ApiError } from "../utils/ApiError.js";
 import { validationResult } from "express-validator";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAuthToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const authToken =  user.generateAuthToken()
+
+        user.authToken = authToken
+        await user.save({ validateBeforeSave : false})
+   
+        return {authToken};
+
+    } catch (error) {
+        console.log(error);
+        throw new ApiError(500," Something Went Wrong While Generating auth Token");
+    }
+}
+
+
 const registerUser = asyncHandler(async (req,res) => {
 
    // get user data from frontend
@@ -22,7 +39,7 @@ const registerUser = asyncHandler(async (req,res) => {
 
    // get user data from frontend
    const {fullName:{firstName, lastName},email,password} = req.body
-//    const {firstName, lastName} = fullName || {};
+    // const {firstName, lastName} = fullName || {};
 
       //validation check if field are not empty
     if ( [firstName,lastName,email,password].some( (field) => field?.trim() === "" ) ) {
@@ -50,15 +67,73 @@ const registerUser = asyncHandler(async (req,res) => {
         throw new ApiError(500,"something went wrong while registering user");
     }
 
+    const createdUser = await User.findById(user._id).select("-password");
+
+    if (!createdUser) {
+        throw new ApiError(500,"something went wrong while registering user");
+    }
+
     const token = user.generateAuthToken();
 
       // return response to frontend
     return res.status(201).json(
-        new ApiResponse(200, { user, token }, "User Registered Successfully")
+        new ApiResponse(200, { createdUser, token }, "User Registered Successfully")
     );
 })
 
 
+const loginUser = asyncHandler(async (req,res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log("Validation Errors Array: ", errors.array()); 
+        throw new ApiError(400, "Validation Error", errors.array());
+    }
+
+    const {email, password} = req.body
+
+    if(!(email && password)){
+        throw new ApiError(401,"Email and Password are required")
+    }
+
+    const user = await User.findOne({email})
+
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid password");
+    }
+
+    const {authToken} = await generateAuthToken(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password -authToken");
+
+    const options = {
+        httpOnly : true,
+        secure : true,
+        sameSite: "None"
+    };
+
+    res.cookie("authToken", authToken, options);
+    res.set("Authorization", `Bearer ${authToken}`);
+
+    return res.status(200).json(
+        new ApiResponse(200, {authToken, loggedInUser}, "User Logged in Successfully")
+    )
+})
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    return res.status(200).json(
+        new ApiResponse(200, req.user, "User Profile Fetched Successfully")
+    )
+})
+
 export {
     registerUser,
+    loginUser,
+    getUserProfile
 }
