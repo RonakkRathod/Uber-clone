1a10ad6289aabe69aa4d51e2d56eb8dcb2c45fe7
Ronak <ronakrathod0022@gmail.com>
2026-04-21 14:59:17 +0530

    add authUser middleware for JWT token verification and user authentication

---
+++ Backend/src/middleware/auth.middleware.js
@@ -0,0 +1,34 @@
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../Models/user.model.js";
+
+
+// create  a middleware to verify JWT token
+export const authUser = asyncHandler(async (req ,res ,next) => {
+  try {
+
+   const authHeader = req.header("Authorization") || "";
+   const bearerToken = authHeader.startsWith("Bearer ") ? authHeader.slice(7) : "";
+   const token =  req.cookies?.authToken || bearerToken
+  
+     if(!token){
+        throw new ApiError(401, "Unauthorized request")
+     }
+      
+   const decodedToken = jwt.verify(token, process.env.AUTH_TOKEN_SECRET)
+  
+    const user =  await User.findById(decodedToken?._id).select("-password -authToken")
+  
+     if (!user) {   
+        throw new ApiError(401,"Invalid Access Token");
+     }
+  
+     req.user = user;
+     next();
+  
+  } catch (error) {
+     throw new ApiError(401, error?.message || "Inavlid Access Token");
+  }
+  
+})
