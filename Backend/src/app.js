63458c646005bd348005ecb8ee5520d08a2584e5
Ronak <ronakrathod0022@gmail.com>
2026-04-21 14:58:34 +0530

    refactor app.js to enhance CORS configuration and add centralized error handling

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,12 +3,14 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors"
+import cookieParser from "cookie-parser";
 import UserRouter from "./routes/user.routes.js";
 
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 7000;
@@ -17,6 +19,16 @@ app.get("/", (req, res) => {
   res.send("Hello Ronak!");
 });
 
+// http://localhost:7000/api/v1/users
 app.use("/api/v1/users", UserRouter);
+
+// Centralized error handler 
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+    errors: err.errors || [],
+  });
+});
 
 export default app;
