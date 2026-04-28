e888b029b078cb9559b53004d410b5f2f73d1735
Ronak <ronakrathod0022@gmail.com>
2026-04-28 22:59:50 +0530

    feat: add getRideFare function for fare calculation with validation

--- Backend/src/controllers/ride.controller.js
+++ Backend/src/controllers/ride.controller.js
@@ -2,6 +2,7 @@ import { createRide } from "../utils/ride.js";
 import { validationResult } from "express-validator";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
+import { getFare } from "../utils/ride.js";
 
 const handleCreateRide = async (req, res) => {
 
@@ -30,6 +31,30 @@ const handleCreateRide = async (req, res) => {
     }
 }
 
+const getRideFare = async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(
+            new ApiError(400, "Validation error", errors.array())
+        );
+    }
+
+    const { pickupLocation, destination } = req.query;
+
+    try {
+        const fare = await getFare(pickupLocation, destination);
+
+        return res.status(200).json(
+            new ApiResponse(200, fare, "Fare calculated successfully")
+        );
+    } catch (error) {
+        throw new ApiError(error?.statusCode || 500, error.message || "Failed to calculate fare");
+    }
+}    
+
+
 export {
-    handleCreateRide
+    handleCreateRide,
+    getRideFare
 }
